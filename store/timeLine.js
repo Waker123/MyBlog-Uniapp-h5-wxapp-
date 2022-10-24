df916611d86042b9cf1e4606a03de56426e79341
Waker <1871187582@qq.com>
2022-10-24 15:19:39 +0800

    timeLine的完成

--- store/timeLine.js
+++ store/timeLine.js
@@ -1,42 +1,47 @@
 import useLocalStorage from '../untils/useLocalstorage.js'
-import {DAY_TO_31,getMonth,getYear} from '../untils/constant.js'
+import { DAY_TO_31, getMonth, getYear } from '../untils/constant.js'
 export default {
-	namespaced:true,
-	mutations:{
-		changeTimeLineState(state,value){
+	namespaced: true,
+	mutations: {
+		changeTimeLineState(state, value) {
 			state.timeLineState = value;
+		},
+		changeTimeLine(state,newValue){
+			const timeStore = useLocalStorage();
+			state.timeList=newValue;
+			timeStore.setItem("TIMELIST",newValue)
 		}
 	},
-	state:{
-		timeList:getDailyData(),
-		timeLineState:false,
+	state: {
+		timeList: getDailyData(),
+		timeLineState: false,
 	},
 }
 
-function getDailyData(){
-	let timeList=[];
+function getDailyData() {
+	let timeList = [];
 	const timeStore = useLocalStorage();
-	if(!timeStore.getItem("TIMELIST")){
-		let count=0;
-		DAY_TO_31.includes(getMonth)?count=31:count=30;
+	if (!timeStore.getItem("TIMELIST")) {
+		let count = 0;
+		DAY_TO_31.includes(getMonth) ? count = 31 : count = 30;
 		setDailyStore(count);
 	}
 	const storeList = timeStore.getItem("TIMELIST");
-	  Object.keys(storeList).forEach((item) => {
-	    timeList.push(storeList[item]);
-	  });
+	Object.keys(storeList).forEach((item) => {
+		timeList.push(storeList[item]);
+	});
 	return timeList;
 }
 
-function setDailyStore(count){
+function setDailyStore(count) {
 	const timeStore = useLocalStorage();
-	let storeList={};
-	for(let i=1;i<count;i++){
+	let storeList = {};
+	for (let i = 1; i <= count; i++) {
 		storeList[i] = {
-		  id: i,
-		  timestamp: `${getYear}-${getMonth}-${i}`,
-		  title: "今日打卡",
-		  commitInfo: "未完成😢😢😢",
+			id: i,
+			timestamp: `${getYear}-${getMonth}-${i}`,
+			title: "今日份打卡",
+			commitInfo: "未完成😢😢",
 		};
 	}
 	timeStore.setItem("TIMELIST", storeList);
